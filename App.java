8fb4ccaff32c4006969de95907bdae9d336b1b3a
Maksim <fenrion301193@gmail.com>
2024-05-19 22:30:48 +0300

    git test 1 commit

---
+++ App.java
@@ -0,0 +1,13 @@
+package org.example.gitHubTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "git test 1" );
+    }
+}
